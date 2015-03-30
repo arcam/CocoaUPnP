af6be47385331f3b573a21944500af6e0651324b
Paul Williamson <PaulW@arcam.co.uk>
2015-03-30 09:46:00 +0100

    Created example project

---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Paul Williamson on 30/03/2015.
+//  Copyright (c) 2015 ARCAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Example
+//
+//  Created by Paul Williamson on 30/03/2015.
+//  Copyright (c) 2015 ARCAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

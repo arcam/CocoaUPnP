b97224f89f842438422a33193e945ae14e587c1c
Paul Williamson <PaulW@arcam.co.uk>
2015-04-16 12:49:51 +0100

    Remove comments

--- CocoaUPnP/Models/UPPMediaItemResource.m
+++ CocoaUPnP/Models/UPPMediaItemResource.m
@@ -1,10 +1,3 @@
-//
-//  UPPMediaItemResource.m
-//  Example
-//
-//  Created by Paul Williamson on 15/04/2015.
-//  Copyright (c) 2015 ARCAM. All rights reserved.
-//
 
 #import "UPPMediaItemResource.h"
 

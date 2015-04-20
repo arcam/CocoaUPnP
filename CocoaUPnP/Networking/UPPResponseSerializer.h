c9188128a8e7fb70b57ddc250e2b4b3cb01e70a3
Paul Williamson <PaulW@arcam.co.uk>
2015-04-20 13:45:13 +0100

    Add empty response serialiser

---
+++ CocoaUPnP/Networking/UPPResponseSerializer.h
@@ -0,0 +1,13 @@
+//
+//  UPPResponseSerializer.h
+//  Pods
+//
+//  Created by Paul Williamson on 20/04/2015.
+//
+//
+
+#import "AFURLResponseSerialization.h"
+
+@interface UPPResponseSerializer : AFXMLParserResponseSerializer
+
+@end

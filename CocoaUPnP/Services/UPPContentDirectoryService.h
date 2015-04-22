def8c0bea413fd13b13365b44e13e67cca929c88
Paul Williamson <PaulW@arcam.co.uk>
2015-04-22 15:59:15 +0100

    Add Content Directory Service

---
+++ CocoaUPnP/Services/UPPContentDirectoryService.h
@@ -0,0 +1,8 @@
+// CocoaUPnP by A&R Cambridge Ltd, http://www.arcam.co.uk
+// Copyright 2015 Arcam. See LICENSE file.
+
+#import "UPPBasicService.h"
+
+@interface UPPContentDirectoryService : UPPBasicService
+
+@end
---
+++ CocoaUPnPTests/Services/UPPContentDirectoryServiceSpec.m
@@ -0,0 +1,82 @@
+// CocoaUPnP by A&R Cambridge Ltd, http://www.arcam.co.uk
+// Copyright 2015 Arcam. See LICENSE file.
+
+#import "UPPContentDirectoryService.h"
+
+SpecBegin(UPPContentDirectoryService)
+
+describe(@"UPPContentDirectoryService", ^{
+    
+    describe(@"when getting search capabilities", ^{
+        xit(@"should send required parameters", ^{
+        });
+    });
+    
+    describe(@"when getting sort capabilities", ^{
+        xit(@"should send required parameters", ^{
+        });
+    });
+    
+    describe(@"when getting system update id", ^{
+        xit(@"should send required parameters", ^{
+        });
+    });
+    
+    describe(@"when browsing directory", ^{
+        xit(@"should send required parameters", ^{
+        });
+    });
+    
+    describe(@"when searching directory", ^{
+        xit(@"should send required parameters", ^{
+        });
+    });
+    
+    describe(@"when creating object", ^{
+        xit(@"should send required parameters", ^{
+        });
+    });
+    
+    describe(@"when destroying object", ^{
+        xit(@"should send required parameters", ^{
+        });
+    });
+    
+    describe(@"when updating object", ^{
+        xit(@"should send required parameters", ^{
+        });
+    });
+    
+    describe(@"when importing resource", ^{
+        xit(@"should send required parameters", ^{
+        });
+    });
+    
+    describe(@"when exporting resource", ^{
+        xit(@"should send required parameters", ^{
+        });
+    });
+    
+    describe(@"when stopping transfer resource", ^{
+        xit(@"should send required parameters", ^{
+        });
+    });
+    
+    describe(@"when getting transfer progress", ^{
+        xit(@"should send required parameters", ^{
+        });
+    });
+    
+    describe(@"when deleting resource", ^{
+        xit(@"should send required parameters", ^{
+        });
+    });
+    
+    describe(@"when creating reference", ^{
+        xit(@"should send required parameters", ^{
+        });
+    });
+    
+});
+
+SpecEnd

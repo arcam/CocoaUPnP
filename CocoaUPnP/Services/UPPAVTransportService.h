0f296684cf89bddb2b874776243d95abcbbdf762
Paul Williamson <PaulW@arcam.co.uk>
2015-04-22 08:51:33 +0100

    Change method signatures for GET calls

--- CocoaUPnP/Services/UPPAVTransportService.h
+++ CocoaUPnP/Services/UPPAVTransportService.h
@@ -52,21 +52,47 @@
 /**
  Get media information from the service
  
- @discussion This method does not return values for PlayMedium, RecordMedium or
-     WriteStatus.
+ @param instanceId The instance identifier
+ @param completion A completion block which returns the parsed response, or an 
+     error if unsuccessful
+ */
+- (void)mediaInfoWithInstanceID:(NSString *)instanceId completion:(void(^)(NSDictionary *mediaInfo, NSError *error))completion;
+
+/**
+ Get transport information from the service
  
  @param instanceId The instance identifier
- @param completion A completion block with the returned data
+ @param completion A completion block which returns the parsed response, or an 
+     error if unsuccessful
  */
-- (void)mediaInfoWithInstanceID:(NSString *)instanceId completion:(void(^)(NSNumber *numberOfTracks, NSString *duration, NSString *currentURI, NSString *currentURIMetaData, NSString *nextURI, NSString *nextURIMetaData))completion;
+- (void)transportInfoWithInstanceID:(NSString *)instanceId completion:(void(^)(NSDictionary *transportInfo, NSError *error))completion;
 
-- (void)transportInfoWithInstanceID:(NSString *)instanceId completion:(void(^)(NSString *currentTransportState, NSString *currentTransportStatus, NSString *currentSpeed))completion;
+/**
+ Get position information from the service
+ 
+ @param instanceId The instance identifier
+ @param completion A completion block which returns the parsed response, or an 
+     error if unsuccessful
+ */
+- (void)positionInfoWithInstanceID:(NSString *)instanceId completion:(void(^)(NSDictionary *positionInfo,  NSError *error))completion;
 
-- (void)positionInfoWithInstanceID:(NSString *)instanceId completion:(void(^)(NSString *track, NSString *trackDuration, NSString *trackMetaData, NSString *trackURI, NSString *relTime, NSString *absTime, NSString *relCount, NSString *absCount))completion;
+/**
+ Get device capabilities from the service
+ 
+ @param instanceId The instance identifier
+ @param completion A completion block which returns the parsed response, or an 
+     error if unsuccessful
+ */
+- (void)deviceCapabilitiesWithInstanceID:(NSString *)instanceId completion:(void(^)(NSDictionary *deviceCapabilities, NSError *error))completion;
 
-- (void)deviceCapabilitiesWithInstanceID:(NSString *)instanceId completion:(void(^)(NSString *playMedia, NSString *recMedia, NSString *recQualityModes))completion;
-
-- (void)transportSettingsWithInstanceID:(NSString *)instanceId completion:(void(^)(NSString *playMode, NSString *recQualityModes))completion;
+/**
+ Get tranport settings from the service
+ 
+ @param instanceId The instance identifier
+ @param completion A completion block which returns the parsed response, or an 
+     error if unsuccessful
+ */
+- (void)transportSettingsWithInstanceID:(NSString *)instanceId completion:(void(^)(NSDictionary *transportSettings, NSError *error))completion;
 
 /**
  Send stop command

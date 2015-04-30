015df76e97b4d2a8f7897f6c02f1aa35cec37b50
Paul Williamson <PaulW@arcam.co.uk>
2015-04-30 14:12:40 +0100

    Session manager should load with serializers

--- CocoaUPnP/Networking/UPPSessionManager.m
+++ CocoaUPnP/Networking/UPPSessionManager.m
@@ -2,7 +2,21 @@
 // Copyright 2015 Arcam. See LICENSE file.
 
 #import "UPPSessionManager.h"
+#import "UPPRequestSerializer.h"
+#import "UPPResponseSerializer.h"
 
 @implementation UPPSessionManager
+
+- (instancetype)initWithBaseURL:(NSURL *)url
+{
+    self = [super initWithBaseURL:url];
+
+    if (self) {
+        self.requestSerializer = [UPPRequestSerializer serializer];
+        self.responseSerializer = [UPPResponseSerializer serializer];
+    }
+
+    return self;
+}
 
 @end
--- CocoaUPnPTests/Networking/UPPSessionManagerSpec.m
+++ CocoaUPnPTests/Networking/UPPSessionManagerSpec.m
@@ -2,20 +2,35 @@
 // Copyright 2015 Arcam. See LICENSE file.
 
 #import "UPPSessionManager.h"
+#import "UPPRequestSerializer.h"
+#import "UPPResponseSerializer.h"
 
 SpecBegin(UPPSessionManager)
 
 describe(@"UPPSessionManager", ^{
     
+    __block UPPSessionManager *manager;
+
+    beforeEach(^{
+        manager = [UPPSessionManager manager];
+    });
+
     it(@"should be instantiated with a base URL", ^{
         NSURL *url = [NSURL URLWithString:@"http://www.google.com"];
-        
+
         UPPSessionManager *manager = [[UPPSessionManager alloc]
                                       initWithBaseURL:url];
-        
+
         expect(manager.baseURL).to.equal(url);
     });
-    
+
+    it(@"should have a request serialiser", ^{
+        expect(manager.requestSerializer).to.beKindOf([UPPRequestSerializer class]);
+    });
+
+    it(@"should have a response serialiser", ^{
+        expect(manager.responseSerializer).to.beKindOf([UPPResponseSerializer class]);
+    });
 });
 
 SpecEnd

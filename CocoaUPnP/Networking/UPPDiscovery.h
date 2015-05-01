effd29e1e269f4639999b9ad6970a3e503923ce9
Paul Williamson <PaulW@arcam.co.uk>
2015-05-01 10:57:35 +0100

    Add comments to discovery

--- CocoaUPnP/Networking/UPPDiscovery.h
+++ CocoaUPnP/Networking/UPPDiscovery.h
@@ -7,18 +7,56 @@
 
 @class UPPDiscovery;
 
+/**
+ By confirming to `UPPDiscoveryDelegate`, a class can be informed whenever a
+ UPnP device is found or removed.
+ */
 @protocol UPPDiscoveryDelegate <NSObject>
 @optional
+
+/**
+ Inform class that a new UPnP device was found: either through responding to an
+ `M-SEARCH` request or from periodic `NOTIFY` messaging to the multicast address.
+ 
+ @param discovery The discovery instance calling the protocol method.
+ @param device    The device that was found.
+ */
 - (void)discovery:(UPPDiscovery *)discovery didFindDevice:(UPPBasicDevice *)device;
+
+/**
+ Inform delegate that a UPnP device was removed, usually by the device sending a
+ `byebye` message.
+ 
+ @param discovery The discovery instance calling the protocol method.
+ @param device    The device that was removed.
+ */
 - (void)discovery:(UPPDiscovery *)discovery didRemoveDevice:(UPPBasicDevice *)device;
 @end
 
+/**
+ This class is responsible for searching for devices over SSDP. It also watches
+ for any `NOTIFY` messages sent by unknown devices.
+ */
 @interface UPPDiscovery : NSObject <SSDPServiceBrowserDelegate>
 
+/**
+ A parser which will parse headers as they come in.
+ */
 @property (strong, nonatomic) UPPDeviceParser *parser;
+
+/**
+ The object who wished to be informed of add/remove device notices.
+ */
 @property (weak, nonatomic) id <UPPDiscoveryDelegate> delegate;
 
+/**
+ Creates and returns a common shared instance.
+ */
 + (UPPDiscovery *)sharedInstance;
+
+/**
+ Return an array of all currently known devices.
+ */
 - (NSArray *)availableDevices;
 
 @end

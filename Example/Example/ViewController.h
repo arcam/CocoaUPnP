bb4702d024ba434406cb4de17e68e7e04cd90f44
Paul Williamson <PaulW@arcam.co.uk>
2015-05-08 14:21:02 +0100

    Create simple view controller using CocoaUPnP

--- Example/Example/ViewController.h
+++ Example/Example/ViewController.h
@@ -3,5 +3,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @end

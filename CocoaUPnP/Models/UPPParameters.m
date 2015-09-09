12aab3733f187e48746de21df0b3fe3ff344162b
Paul Williamson <PaulW@arcam.co.uk>
2015-09-09 12:31:14 +0100

    Created a parameters model

---
+++ CocoaUPnP/Models/UPPParameters.m
@@ -0,0 +1,55 @@
+// CocoaUPnP by A&R Cambridge Ltd, http://www.arcam.co.uk
+// Copyright 2015 Arcam. See LICENSE file.
+
+#import "UPPParameters.h"
+
+@interface UPPParameters ()
+@property (copy, nonatomic) NSArray *keys;
+@property (copy, nonatomic) NSArray *values;
+@end
+
+@implementation UPPParameters
+
++ (instancetype)paramsWithKeys:(NSArray *)keys values:(NSArray *)values
+{
+    return [[[self class] alloc] initWithKeys:keys values:values];
+}
+
+- (instancetype)initWithKeys:(NSArray *)keys values:(NSArray *)values
+{
+    if ((self = [super init])) {
+        if (keys.count != values.count) {
+            [NSException raise:@"Key value counts do not match" format:nil];
+        }
+
+        self.keys = keys;
+        self.values = values;
+    }
+    return self;
+}
+
+- (void)enumerateKeysAndValuesUsingBlock:(void (^)(id key, id value, NSUInteger idx, BOOL *stop))block
+{
+    [self.keys enumerateObjectsUsingBlock:^(id key, NSUInteger idx, BOOL *stop) {
+        block(key, self.values[idx], idx, stop);
+    }];
+}
+
+- (BOOL)isEqual:(UPPParameters *)object
+{
+    if (!object || ![object isKindOfClass:[UPPParameters class]]) {
+        return NO;
+    }
+
+    if (![self.keys isEqualToArray:object.keys]) {
+        return NO;
+    }
+
+    if (![self.values isEqualToArray:object.values]) {
+        return NO;
+    }
+
+    return YES;
+}
+
+@end

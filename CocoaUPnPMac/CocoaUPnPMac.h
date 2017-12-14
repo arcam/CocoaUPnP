8a9e98fe33c897505c82c4ed016268c5a48be963
Daniel K. Poulsen <daniel.poulsen@cambridgeaudio.com>
2017-12-14 20:14:25 +0100

    import

---
+++ CocoaUPnPMac/CocoaUPnPMac.h
@@ -0,0 +1,19 @@
+//
+//  CocoaUPnPMac.h
+//  CocoaUPnPMac
+//
+//  Created by Daniel K. Poulsen on 14/12/2017.
+//  Copyright © 2017 cambridgeaudio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CocoaUPnPMac.
+FOUNDATION_EXPORT double CocoaUPnPMacVersionNumber;
+
+//! Project version string for CocoaUPnPMac.
+FOUNDATION_EXPORT const unsigned char CocoaUPnPMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaUPnPMac/PublicHeader.h>
+
+

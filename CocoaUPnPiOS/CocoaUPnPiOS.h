f22d58a82a79214cd0dee765c92896687683e613
Daniel K. Poulsen <daniel.poulsen@cambridgeaudio.com>
2017-12-14 13:45:30 +0100

    fixed Carthage support

---
+++ CocoaUPnPiOS/CocoaUPnPiOS.h
@@ -0,0 +1,19 @@
+//
+//  CocoaUPnPiOS.h
+//  CocoaUPnPiOS
+//
+//  Created by Daniel K. Poulsen on 14/12/2017.
+//  Copyright © 2017 cambridgeaudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaUPnPiOS.
+FOUNDATION_EXPORT double CocoaUPnPiOSVersionNumber;
+
+//! Project version string for CocoaUPnPiOS.
+FOUNDATION_EXPORT const unsigned char CocoaUPnPiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaUPnPiOS/PublicHeader.h>
+
+
